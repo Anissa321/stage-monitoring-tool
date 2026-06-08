9fe84c5716a442e2030e918c2a8f9a3d52eb186a
Anissa321 <anissa.canton-rodriguez@student.ehb.be>
2026-06-08 11:28:41 +0200

    Setup frontend project structure and login page

---
+++ frontend/src/pages/auth/LoginPage.vue
@@ -0,0 +1,50 @@
+<template>
+  <main class="login-page">
+    <section class="login-card">
+      <h1>Stage Monitoring Tool</h1>
+
+      <form class="login-form">
+        <label for="email">Email</label>
+        <input id="email" type="email" />
+
+        <label for="password">Wachtwoord</label>
+        <input id="password" type="password" />
+
+        <button type="button">Login</button>
+      </form>
+    </section>
+  </main>
+</template>
+
+<script setup>
+</script>
+
+<style scoped>
+.login-page {
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.login-card {
+  width: 360px;
+  text-align: center;
+}
+
+.login-form {
+  display: flex;
+  flex-direction: column;
+  gap: 12px;
+  margin-top: 80px;
+}
+
+input {
+  height: 35px;
+}
+
+button {
+  height: 45px;
+  margin-top: 20px;
+}
+</style>
